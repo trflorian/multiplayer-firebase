9eb8352f66d7ef41adde2ef84cc4acd139626333
trflorian <florian.trautweiler@hispeed.ch>
2024-09-06 00:13:15 +0200

    added comments to urls

--- scripts/firebase_urls.gd
+++ scripts/firebase_urls.gd
@@ -1,9 +1,11 @@
 extends Node
 
+# For a real Firebase Realtime Database
 const HOST: String = "https://godot-multiplayer-firebase-default-rtdb.europe-west1.firebasedatabase.app"
 const PORT: int = 443
 
-#const HOST: String = "http://127.0.0.1"
+# For local Realtime Database mock server, see https://github.com/trflorian/realtimedb-local-server
+#const HOST: String = "http://localhost"
 #const PORT: int = 8000
 
 func get_player_url(player_id: int) -> String:
